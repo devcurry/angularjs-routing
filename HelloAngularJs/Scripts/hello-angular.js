2ddf794de3328afd0cb815e7604aa5cfbfbbeb7f
sumitkm <sumitkm@gmail.com>
2013-08-08 02:15:38 +0530

    Code Commit

---
+++ HelloAngularJs/Scripts/hello-angular.js
@@ -0,0 +1,111 @@
+﻿/// <reference path="jquery-2.0.3.js" />
+/// <reference path="hello-angular.js" />
+/// <reference path="angular.js" />
+
+// Create ngTwitter Module (roughly Module = namespace in C#)
+
+var ngTwitter = angular.module("ngTwitter", ['ngResource']);
+
+ngTwitter.config(function ($routeProvider)
+{
+    $routeProvider.when(
+        "/", {
+            templateUrl: "timeline",
+            controller: "TimelineController"
+        }).when(
+        "/status/:id", {
+            templateUrl: "status",
+            controller: "TimelineController"
+        })
+});
+
+// Declaring a Service
+ngTwitter.factory("TwitterService", function ($resource, $http, $routeParams)
+{
+    return {
+        timeline: $resource("/Home/Tweet"),
+        status: function (id)
+        {
+            return $http.get("/Home/Status/" + id);
+        }
+    }
+});
+
+ngTwitter.directive("retweetButton", function ()
+{
+    return {
+        restrict: "E",
+        replace: true,
+        scope: {
+            text: "@",
+            clickevent: "&"
+        },
+        template: "<button class='btn btn-mini' ng-click='clickevent()'><i class='icon-retweet'></i> {{text}}</button>"
+    };
+});
+
+//ngTwitter.controller("StatusController", function ($scope, $http, TwitterService)
+//{
+//    $http.get("/Home/Status/" + $routeParams.id)
+//    statusPromise.success(function (data)
+//    {   
+//        $scope.status = data;
+//    });
+//});
+
+ngTwitter.controller("TimelineController", function ($scope, $http, $routeParams, TwitterService)
+{
+    if ($routeParams.id)
+    {
+        var statusPromise = TwitterService.status($routeParams.id);
+        statusPromise.success(function (data)
+        {
+            $scope.status = data;
+        });
+    }
+    else
+    {
+        $scope.tweets = TwitterService.timeline.query({});
+    }
+    $scope.newTweets = {
+        0: "No new Tweets",
+        other: "{} new Tweets"
+    };
+
+    $scope.sendStatus = function ()
+    {
+        var tweetText = $scope.statusText;
+        var newTimeLine = new TwitterService.timeline(
+            {
+                Tweet: tweetText
+            });
+        newTimeLine.$save(function (data, headers)
+        {
+            if (data.success && data.success == true)
+            {
+                alert("Tweet Sent Successfully!");
+                $scope.statusText = "";
+            }
+            else
+            {
+                alert("ERROR: " + data.errorMessage);
+            }
+        });
+    };
+
+    $scope.retweet = function (item)
+    {
+        var resultPromise = $http.post("/Home/Retweet/", item);
+        resultPromise.success(function (data)
+        {
+            if (data.success)
+            {
+                alert("Retweeted successfully");
+            }
+            else
+            {
+                alert("ERROR: Retweeted failed! " + data.errorMessage);
+            }
+        });
+    };
+});
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-angularjs-routing
+AngularJS - Routing
 =================
 
-A demo on getting started with Routing in AngularJS
+The 5th post in the AngularJS series showing how to get started with Routing in AngularJS. This continues to use the ASP.NET MVC Twitter client application and adds a new Status 'View'.
+
+Previous articles in the series
+-----------------
+Part 1 - [Hello AngularJS – Building a Tweet Reader](http://www.devcurry.com/2013/06/hello-angularjs-building-tweet-reader.html)
+
+Part 2 - [Using AngularJS Modules and Services Feature in an ASP.NET MVC application](http://www.devcurry.com/2013/06/using-angularjs-modules-and-services.html)
+
+Part 3 - [AngularJS – Post data using the $resource Service in an ASP.NET MVC app](http://www.devcurry.com/2013/07/angularjs-post-data-using-resource.html)
+
+Part 4 - [Using Custom Directive in AngularJS to create reusable JavaScript components for your ASP.NET MVC app](http://www.devcurry.com/2013/08/using-custom-directive-in-angularjs-to.html)
